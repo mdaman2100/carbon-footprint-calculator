4c6a23353953eb945a938a5323f89c88e737e409
Mohd Aman <148184025+mdaman2100@users.noreply.github.com>
2025-01-09 19:19:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const startButton = document.getElementById('startButton');
+
+startButton.addEventListener('click', () => {
+    // Handle button click event (e.g., display a modal, start the calculation)
+    alert("Calculation Started!"); 
+});
